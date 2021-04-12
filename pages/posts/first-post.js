8fa9c29d22a529ec19e7355874107b4eacf4df71
clairan <andreas.clair@gmail.com>
2021-04-12 21:05:00 +0200

    css components 1

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
     return  (
-        <>
+        <Layout> 
+        <Head>
+            <title>First post</title>
+        </Head>
         <h1>First post</h1>
         <h2>
             <Link href ="/">
                 <a>back to home</a>
             </Link>
         </h2>
-        </>
+        </Layout>
     )
     
 }
